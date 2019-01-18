34bd69f82a8d25c2a32d46066d7d75ff28b295bd
Matthias Osswald <m@osswald.li>
2019-01-18 16:43:54 +0100

    Build test environment more structured.

--- ftw/linkchecker/test_setup.py
+++ ftw/linkchecker/test_setup.py
@@ -9,94 +9,166 @@ def set_up_test_environment(portal):
     """Set up two plone sites. Both plone sites having the same content.
     Plone sites have working and broken relations and external links.
     """
-    broken_link = portal.absolute_url() + '/gibtsnicht'
-    working_link = portal.absolute_url()
 
     setSite(portal)
     pages = [create(
         Builder('sl content page').titled(u'Page {}'.format(index)))
         for index in range(9)]
 
-    # Test 1 setup
-    for index, url in enumerate([broken_link, working_link]):
-        add_sl_textblock_having_external_link(pages[0], url, index)
-
-    # Test 2 setup
-    create(Builder('sl textblock')
-           .within(pages[0])
-           .titled('default title')
-           .having(internal_link=RelationValue(pages[1])))
     # create broken relation value
     broken_rel = RelationValue(pages[2])
     broken_rel.to_id = None
-    create(Builder('sl textblock')
-           .within(pages[0])
-           .titled('broken relation')
-           .having(internal_link=broken_rel))
 
-    # Test 3 setup
-    create(Builder('sl content page')
-           .within(pages[3])
-           .titled(u'Content page on page 3'))
-    add_link_into_textarea_without_using_the_browser(
-        pages[3],
-        'content-page-on-page-3')
-    add_link_into_textarea_without_using_the_browser(
-        pages[3],
-        'Idunnoexist')
+    content_for_the_setup = [
+        {
+            'test_number': '1',
+            'content_type': 'sl textblock',
+            'within': pages[0],
+            'title': '1',
+            'int_url': None,
+            'ext_url': portal.absolute_url(),
+            'text': None,
+            'textblock_within1': None,
+            'textblock_url1': None,
+            'textblock_within2': None,
+            'textblock_url2': None,
+        },
+        {
+            'test_number': '1',
+            'content_type': 'sl textblock',
+            'within': pages[0],
+            'title': '0',
+            'int_url': None,
+            'ext_url': portal.absolute_url() + '/gibtsnicht',
+            'text': None,
+            'textblock_within1': None,
+            'textblock_url1': None,
+            'textblock_within2': None,
+            'textblock_url2': None,
+        },
+        {
+            'test_number': '2',
+            'content_type': 'sl textblock',
+            'within': pages[0],
+            'title': 'default title',
+            'int_url': RelationValue(pages[1]),
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': None,
+            'textblock_url1': None,
+            'textblock_within2': None,
+            'textblock_url2': None,
+        },
+        {
+            'test_number': '2',
+            'content_type': 'sl textblock',
+            'within': pages[0],
+            'title': 'broken relation',
+            'int_url': broken_rel,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': None,
+            'textblock_url1': None,
+            'textblock_within2': None,
+            'textblock_url2': None,
+        },
+        {
+            'test_number': '3',
+            'content_type': 'sl content page',
+            'within': pages[3],
+            'title': 'Content page on page 3',
+            'int_url': None,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': pages[3],
+            'textblock_url1': 'content-page-on-page-3',
+            'textblock_within2': pages[3],
+            'textblock_url2': 'Idunnoexist',
+        },
+        {
+            'test_number': '4',
+            'content_type': 'sl content page',
+            'within': pages[4],
+            'title': 'Content page on page 4',
+            'int_url': None,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': pages[4],
+            'textblock_url1': './content-page-on-page-4',
+            'textblock_within2': pages[4],
+            'textblock_url2': './Icantbefound',
+        },
+        {
+            'test_number': '5',
+            'content_type': 'sl content page',
+            'within': pages[5],
+            'title': 'Content page on page 5',
+            'int_url': None,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': pages[5],
+            'textblock_url1': '/page-5',
+            'textblock_within2': pages[5],
+            'textblock_url2': '/Iwasnevercreated',
+        },
+        {
+            'test_number': '6',
+            'content_type': 'sl content page',
+            'within': pages[6],
+            'title': 'Content page on page 6',
+            'int_url': None,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': pages[6],
+            'textblock_url1': 'resolveuid/' + pages[8].UID(),
+            'textblock_within2': pages[6],
+            'textblock_url2': 'resolveuid/99999999999999999999999999999999',
+        },
+        {
+            'test_number': '7',
+            'content_type': 'sl content page',
+            'within': pages[7],
+            'title': 'Content page on page 7',
+            'int_url': None,
+            'ext_url': None,
+            'text': None,
+            'textblock_within1': pages[7],
+            'textblock_url1': portal.absolute_url(),
+            'textblock_within2': pages[7],
+            'textblock_url2': portal.absolute_url() + '/Sadnottoexist',
+        },
+    ]
 
-    # Test 4 setup
-    create(Builder('sl content page')
-           .within(pages[4])
-           .titled(u'Content page on page 4'))
-    add_link_into_textarea_without_using_the_browser(
-        pages[4],
-        './content-page-on-page-4')
-    add_link_into_textarea_without_using_the_browser(
-        pages[4],
-        './Icantbefound')
-
-    # Test 5 setup
-    create(Builder('sl content page')
-           .within(pages[5])
-           .titled(u'Content page on page 5'))
-    add_link_into_textarea_without_using_the_browser(
-        pages[5],
-        '/page-5')
-    add_link_into_textarea_without_using_the_browser(
-        pages[5],
-        '/Iwasnevercreated')
-
-    # Test 6 setup
-    create(Builder('sl content page')
-           .within(pages[6])
-           .titled(u'Content page on page 6'))
-    valid_uid = 'resolveuid/' + pages[8].UID()
-    broken_uid = 'resolveuid/99999999999999999999999999999999'
-    add_link_into_textarea_without_using_the_browser(
-        pages[6],
-        valid_uid)
-    add_link_into_textarea_without_using_the_browser(
-        pages[6],
-        broken_uid)
-
-    # Test 7 setup
-    create(Builder('sl content page')
-           .within(pages[7])
-           .titled(u'Content page on page 7'))
-    add_link_into_textarea_without_using_the_browser(
-        pages[7],
-        portal.absolute_url())
-    add_link_into_textarea_without_using_the_browser(
-        pages[7],
-        portal.absolute_url() + '/Sadnottoexist')
+    for content in content_for_the_setup:
+        make_content_for_me(content['content_type'],
+                            content['within'],
+                            content['title'],
+                            content['int_url'],
+                            content['ext_url'],
+                            content['text'],
+                            content['textblock_within1'],
+                            content['textblock_url1'],
+                            content['textblock_within2'],
+                            content['textblock_url2'], )
 
 
-def add_sl_textblock_having_external_link(current_page, link, index):
-    create(Builder('sl textblock')
-           .within(current_page)
-           .having(external_link=link)
-           .titled(str(index).decode("utf-8")))
+def make_content_for_me(content_type, within, title, int_url, ext_url, text,
+                        textblock_within1, textblock_url1, textblock_within2,
+                        textblock_url2):
+    create(Builder(content_type)
+           .within(within)
+           .titled(title.decode('utf-8'))
+           .having(internal_link=int_url,
+                   external_link=ext_url,
+                   text=text))
+
+    if textblock_within1 and textblock_url1:
+        add_link_into_textarea_without_using_the_browser(textblock_within1,
+                                                         textblock_url1)
+
+    if textblock_within2 and textblock_url2:
+        add_link_into_textarea_without_using_the_browser(textblock_within2,
+                                                         textblock_url2)
 
 
 def add_link_into_textarea_without_using_the_browser(page, url):
