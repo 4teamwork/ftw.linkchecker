bbd22363611b68e7c8764359ff65a2c2087c9412
Matthias Osswald <m@osswald.li>
2019-01-22 15:19:40 +0100

    Introduce BrokenLink class initializing link information.

---
+++ ftw/linkchecker/command/broken_link.py
@@ -0,0 +1,11 @@
+class BrokenLink(object):
+    def __init__(self):
+        self.is_internal = 'Unknown'
+        self.link_origin = 'Unknown'
+        self.link_target = 'Unknown'
+        self.status_code = 'Unknown'
+        self.content_type = 'Unknown'
+        self.response_time = 'Unknown'
+        self.header_location = 'Unknown'
+        self.error_message = 'Unknown'
+        self.is_broken = 'Unknown'
