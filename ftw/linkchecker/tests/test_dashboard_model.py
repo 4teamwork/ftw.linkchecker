c5862ec9ed4f7937f8ddeadb4f5d8e39debd6177
Matthias Osswald <m@osswald.li>
2020-04-28 21:07:04 +0200

    Implement data extractor for excel reports.
    The change lets one extract the excel report data from within a report in the file listing block configured.
    
    I chose to import the data into a pandas dataframe because that makes the data filterable in powerful ways.

--- ftw/linkchecker/tests/test_dashboard_model.py
+++ ftw/linkchecker/tests/test_dashboard_model.py
@@ -1,5 +1,16 @@
+from ftw.builder import Builder
+from ftw.builder import create
 from ftw.linkchecker.browser.dashboard import Dashboard
 from ftw.linkchecker.tests import FunctionalTestCase
+from plone import api
+import os
+import pandas as pd
+import transaction
+
+
+ASSET_NAME = 'linkchecker_report_2020_Feb_28_130820.xlsx'
+TEST_FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
+TEST_REPORT = os.path.join(TEST_FOLDER_PATH, 'exemplar_data', ASSET_NAME)
 
 
 class TestDashboardFunctional(FunctionalTestCase):
@@ -28,3 +39,32 @@ class TestDashboardFunctional(FunctionalTestCase):
             expected, dashboard.dashboard_model._get_annotation(),
             'It is expected that the annotation value can be set and get.')
 
+    def test_read_data_from_excel_report(self):
+        self.create_file_listing_block_with_report()
+        dashboard = Dashboard(self.portal, self.request)
+
+        # prepare dataframes to compare
+        df_expected = pd.read_excel(TEST_REPORT)
+        df_actual = dashboard.dashboard_model._get_latest_report_from_excel()
+
+        pd.testing.assert_frame_equal(df_expected, df_actual)
+
+    def create_file_listing_block_with_report(self):
+        # build structure so that there is an excel report at
+        # linkchecker/reports which is the default in registry.xml
+        linkchecker_page = create(Builder('sl content page')
+                                  .titled(u'linkchecker')
+                                  .within(self.portal))
+        file_listing_block = create(Builder('sl listingblock')
+                                    .titled(u'reports')
+                                    .within(linkchecker_page))
+
+        # upload test asset to the file listing block
+        with open(TEST_REPORT, 'rb') as f_:
+            f_.seek(0)
+            data = f_.read()
+        file_ = api.content.create(
+            container=file_listing_block, type='File',
+            title=ASSET_NAME, file=data)
+        file_.setFilename(ASSET_NAME)
+        transaction.commit()
