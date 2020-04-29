9bf5798a74f33e555d4b8718d9214c60ad891c11
Matthias Osswald <m@osswald.li>
2020-04-29 10:41:00 +0200

    Check if there is a more recent report.
    If there is data in the annotations a check is done if a more recent report exists in the filelisting block configured. If that's the case this data is loaded into a df.

--- ftw/linkchecker/browser/dashboard.py
+++ ftw/linkchecker/browser/dashboard.py
@@ -20,6 +20,18 @@ class DashboardModel(object):
         self.request = request
         self.context = context
 
+        self._persisted_data = self._get_annotation()
+        self._timestamp = self._persisted_data.get('timestamp', 0)
+        self._reports = self._load_and_sort_fileblock_reports()
+
+        self._latest_report_df = self._get_latest_report_df_from_excel()
+        if isinstance(self._latest_report_df, pd.DataFrame):
+            # there is new data -> update/merge data
+            pass
+        else:
+            # no data or no new data found
+            pass
+
     def _get_report_path(self):
         return api.portal.get_registry_record(
             'ftw.linkchecker.dashboard_storage')
@@ -30,22 +42,41 @@ class DashboardModel(object):
 
     def _get_annotation(self):
         annotations = IAnnotations(self.context)
-        return annotations['ftw.linkchecker.link_data']
+        try:
+            return annotations['ftw.linkchecker.link_data']
+        except KeyError:
+            return {}
 
-    def _get_latest_report_from_excel(self):
+    def _has_more_recent_report(self):
+        if not self._reports:
+            return
+
+        dt_current = datetime.fromtimestamp(self._timestamp)
+        if self._reports[0][0] > dt_current:
+            return True
+
+    def _load_and_sort_fileblock_reports(self):
         portal = api.portal.get()
         report_path = self._get_report_path().encode('utf-8')
-        file_listing_block = portal.unrestrictedTraverse(report_path)
+        try:
+            file_listing_block = portal.unrestrictedTraverse(report_path)
+        except KeyError:
+            return
         reports = file_listing_block.items()
+        if not reports:
+            return
         # sort by date in file name (latest first)
         reports = [
             (datetime.strptime(report[0][19:-5], '%Y_%b_%d_%H%M%S'), report[1])
             for report in reports]
         reports.sort(key=lambda x: x[0], reverse=True)
 
-        # TODO: do not read data into memory if there is no new report
+        return reports
 
-        report_date, report = reports[0]
+    def _get_latest_report_df_from_excel(self):
+        if not self._reports or not self._has_more_recent_report():
+            return
+        report_date, report = self._reports[0]
         return pd.read_excel(io.BytesIO(report.get_data()))
 
 
