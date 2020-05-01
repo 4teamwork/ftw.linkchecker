1bfe463a86ab91c1dd8dc16e764764569e76aed6
Matthias Osswald <m@osswald.li>
2020-05-01 09:19:13 +0200

    Add styles to dashboard.
    So that the dashboard looks more or less useful this commit applies some styling.

--- ftw/linkchecker/browser/resources/general.js
+++ ftw/linkchecker/browser/resources/general.js
@@ -2,5 +2,7 @@ $(function() {
     $('.linkchecker-toggle').click(function() {
         $(this).next('.hide-show').toggle();
         $('.hide-show').not($(this).next('.hide-show')).hide();
+
+        $('span', this).toggleClass('icon-arrow-up icon-arrow-down');
     });
 });
